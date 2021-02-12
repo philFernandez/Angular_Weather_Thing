b5a50c27a3e52da96db1023904fa4145e3456810
Phil Fernandez <norcal618@gmail.com>
2021-02-11 23:28:58 -0800

    pull from weather api

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ICurrentWeather } from "../interfaces";
+import { WeatherService } from "../weather/weather.service";
 
 @Component({
     selector: "app-current-weather",
@@ -8,15 +9,10 @@ import { ICurrentWeather } from "../interfaces";
 })
 export class CurrentWeatherComponent implements OnInit {
     current: ICurrentWeather;
-    constructor() {
-        this.current = {
-            city: "Sacramento",
-            country: "USA",
-            date: new Date(),
-            image: "assets/img/sunny.jpg",
-            temperature: 72,
-            description: "sunny",
-        } as ICurrentWeather;
+    constructor(private weatherService: WeatherService) {}
+    ngOnInit(): void {
+        this.weatherService
+            .getCurrentWeather("Sacramento", "US")
+            .subscribe((data) => (this.current = data));
     }
-    ngOnInit(): void {}
 }
