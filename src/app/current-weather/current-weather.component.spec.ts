96471ce8348dd12460c37d76b9c73d410f7a8df4
Phil Fernandez <norcal618@gmail.com>
2021-02-12 21:17:53 -0800

    last of tests

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, inject, TestBed, waitForAsync } from "@angular/core/testing";
 import { CurrentWeatherComponent } from "./current-weather.component";
 import { WeatherService } from "../weather/weather.service";
-import { WeatherServiceFake } from "../weather/weather.service.fake";
+import { fakeWeather, WeatherServiceFake } from "../weather/weather.service.fake";
 import { injectSpy } from "angular-unit-test-helper";
 import { of } from "rxjs";
+import { By } from "@angular/platform-browser";
 
 describe("CurrentWeatherComponent", () => {
     let component: CurrentWeatherComponent;
@@ -34,5 +35,26 @@ describe("CurrentWeatherComponent", () => {
         weatherServiceMock.getCurrentWeather.and.returnValue(of());
         fixture.detectChanges();
         expect(component).toBeTruthy();
+    });
+
+    it("should get currentWeather from weatherService", () => {
+        weatherServiceMock.getCurrentWeather.and.returnValue(of());
+        fixture.detectChanges();
+        expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
+    });
+
+    it("should eagerly load currentWeather in Sacramento from weatherService", () => {
+        // arrange
+        weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+        // act
+        fixture.detectChanges();
+        // assert
+        expect(component.current).toBeDefined();
+        expect(component.current.city).toEqual("Sacramento");
+        expect(component.current.temperature).toEqual(280.32);
+        // assert dom
+        const debugE1 = fixture.debugElement;
+        const titleE1: HTMLElement = debugE1.query(By.css("span")).nativeElement;
+        expect(titleE1.textContent).toContain("Sacramento");
     });
 });
