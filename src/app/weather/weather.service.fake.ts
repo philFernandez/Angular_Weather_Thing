a9c6859a106c0928d93e36740af7d58741f47a4c
Phil Fernandez <norcal618@gmail.com>
2021-02-12 17:26:36 -0800

    fakeWeaterService for test: forgot to add file

---
+++ src/app/weather/weather.service.fake.ts
@@ -0,0 +1,18 @@
+import { Observable, of } from "rxjs";
+import { ICurrentWeather } from "../interfaces";
+import { IWeatherService } from "./weather.service";
+
+export const fakeWeather: ICurrentWeather = {
+    city: "Sacramento",
+    country: "US",
+    date: 1485789600,
+    image: "",
+    temperature: 280.32,
+    description: "Hellfire like storms",
+};
+
+export class WeatherServiceFake implements IWeatherService {
+    getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+        return of(fakeWeather);
+    }
+}
