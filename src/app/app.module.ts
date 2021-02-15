5a52cb7776b939bfeeeddd43156de778853b1807
Phil Fernandez <norcal618@gmail.com>
2021-02-14 23:42:56 -0800

    cant push to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
     declarations: [AppComponent, CurrentWeatherComponent],
-    imports: [BrowserModule, HttpClientModule],
+    imports: [BrowserModule, HttpClientModule, MaterialModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
