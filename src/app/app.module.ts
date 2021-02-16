24efe43a677ebf596d89a213c159d7b4164f54e8
Phil Fernandez <norcal618@gmail.com>
2021-02-16 01:10:50 -0800

    app component material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
     declarations: [AppComponent, CurrentWeatherComponent],
-    imports: [BrowserModule, HttpClientModule, MaterialModule, BrowserAnimationsModule],
+    imports: [
+        FlexLayoutModule,
+        BrowserModule,
+        HttpClientModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
